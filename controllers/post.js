708beb28b0a4c4f6c9c3e06aafd8022b26f2bf2c
Amruta <amrutahatwalne@gmail.com>
2023-02-11 15:38:41 +0530

    kdg

---
+++ controllers/post.js
@@ -0,0 +1,11 @@
+const getAllPost = async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("server Error");
+  }
+};
+
+module.exports = getAllPost;
